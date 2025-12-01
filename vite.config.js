4076945847050cdc8227767d9a996e169e813f39
shauryaupadhyaya <shaurya.upadhyaya@myglobal.school>
2025-12-01 21:54:09 +0800

    Clean Vite + React app, remove GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/forto/' : '/',
+  base: '/',
   plugins: [react()],
 })
