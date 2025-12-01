7567726bb116c3ef9bf65db3b28a235f2f8e1086
shauryaupadhyaya <shaurya.upadhyaya@myglobal.school>
2025-12-01 21:07:38 +0800

    package fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/forto/',
+  base: process.env.NODE_ENV === 'production' ? '/forto/' : '/',
   plugins: [react()],
 })
